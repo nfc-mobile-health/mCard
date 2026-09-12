18c8c76f797d82ca6b9ccab1895a65a3465fb012
Aaradhya Bhardwaj <174193320+FrankTheSssnake@users.noreply.github.com>
2026-09-12 21:39:49 +0530

    initialise empty directory

--- build.gradle.kts
+++
@@ -1,8 +0,0 @@
-// Top-level build.gradle.kts
-plugins {
-    // Change the version here
-    id("com.android.application") version "8.4.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false // It's also a good idea to keep the Kotlin plugin updated
-    id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
-}
-    
